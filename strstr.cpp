140e634e2e47ef016e6ac788ec39d6a131edfd94
sbarkadev <sbarka.dev@gmail.com>
2021-12-15 16:23:47 +0100

    solve strstr problem in cpp

---
+++ strstr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int strStr(std::string haystack, std::string needle)
+{
+    if(needle == "")
+        return 0;
+    int i = haystack.find(needle);
+    return i;
+
+}
+int main()
+{
+    std::cout << strStr("hello","ll") << "\n";
+    std::cout << strStr("hello","llaa") << "\n";
+    std::cout << strStr("aaaa","bba") << "\n";
+    std::cout << strStr("","") << "\n";
+}
